d662069d26869f6f60e60b1b9f32439c45ad1832
Santiago Basulto <santiago.basulto@gmail.com>
2016-05-28 12:38:25 -0300

    Fix ass files

--- unit-13-file-handling/lesson-2-first-line-from-file/tests.py
+++ unit-13-file-handling/lesson-2-first-line-from-file/tests.py
@@ -1,13 +1,17 @@
 import unittest
-
+import tempfile
 
 class AssignmentTestCase(unittest.TestCase):
     def setUp(self):
-        with open('test-file.txt', 'w') as f:
-            f.write('this is line 1\n')
-            f.write('this is line 2\n')
-            f.write('this is line 3\n')
+        self.fp = tempfile.NamedTemporaryFile(mode="w")
+        self.fp.write('this is line 1\n')
+        self.fp.write('this is line 2\n')
+        self.fp.write('this is line 3\n')
+        self.fp.flush()
 
     def test_reads_first_line(self):
-        self.assertEqual(read_first_line('test-file.txt'),
+        self.assertEqual(read_first_line(self.fp.name),
                          'this is line 1\n')
+
+    def tearDown(self):
+        self.fp.close()
